5a31cc7bcdc3921bca78609c592f159560d7409b
mumer2 <umermalik8280@gmail.com>
2024-11-19 17:46:43 +0500

    Layout

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="text-center m-3">
-    <h1 class="font-bold">About</h1>
-    <P class="text-gray-400">Coming Soon...</P>
+    <h1 class="font-bold text-2xl">About Us</h1>
+  </div>
+  <div class="m-10">
+    <h2 class="font-bold">Who We Are:</h2>
+    <p class="text-gray-700">Welcome! We are passionate about helping individuals and professionals create stunning resumes, portfolios, and documents that leave lasting impressions. Our platform offers a range of customizable templates designed to suit every style and profession.</p>
+  <br/>
+  <h2 class="font-bold">Our Mission</h2>
+  <p class="text-gray-700">Our mission is to empower users with tools and templates that are not only aesthetically pleasing but also optimized for showcasing their skills, experiences, and creativity. We believe that everyone deserves to stand out in today’s competitive world.</p>
   </div>
 </template>
 
